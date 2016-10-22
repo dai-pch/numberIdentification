aae0ea4d5b089494443db0e1454b43541bf2e073
ChenJiyang <chen@silensmile.(none)>
2016-10-22 19:39:07 +0800

    main.cpp中注释

--- numberIdentification/main.cpp
+++ numberIdentification/main.cpp
@@ -5,26 +5,29 @@
 
 int main()
 {
-	/***************************读入图片文件以及初始化*****************************/
+	/**************************************************************************/
+	/*                          读入图片文件以及初始化                            */
 	const Mat src = imread("./img/src.jpg");
 	Mat src_changeable;
 	src.copyTo(src_changeable);
-
-
+	/**************************************************************************/
 	//for test
 	//vector<Mat> temptest;
 	//preProcess(src, temptest);
-
+	/**************************************************************************/
+	/*                               图片分割                                  */
 	//查找区域
 	vector<Rect> stringArea = findStringArea(src);
-	
+
 	//从上到下序号排序
 	stringArea = sortStringArea(stringArea);
-	//查看找到的区域
+
+	//查看：展示找到的区域，用红色框框出
 	for (int i = 0; i < stringArea.size(); i++)
 		rectangle(src_changeable, stringArea[i], Scalar(CV_RGB(255, 0, 0)), 2, 8, 0);
+	imshow("show number sequence area", src_changeable);
 
-	//分离区域
+	//分离区域，将每串数字区域存入parts矢量，为三通道未处理图片
 	vector<Mat> parts;
 	vector<Mat> parts_changeable(stringArea.size());
 	parts.clear();
@@ -37,7 +40,8 @@ int main()
 
 	//寻找单个区域中的单个数字
 	vector<vector<Rect>> numbers(parts.size());
-	//findNumberAreaWay == 0 手动阈值化，需配合enhanceStringArea使用
+
+	//findNumberAreaWay == 0手动阈值化，需配合enhanceStringArea，后舍弃（此处保留代码以便可能的使用）
 	//numbers[0] = findNumberArea(parts_changeable[0], 190, Size(10, 2), Size(2, 300));
 	//numbers[1] = findNumberArea(parts_changeable[1], 155, Size(10, 2), Size(2, 300));
 	//numbers[2] = findNumberArea(parts_changeable[2], 150, Size(2, 11), Size(2, 300));
@@ -46,44 +50,35 @@ int main()
 	for(int i = 0; i < numbers.size();i++)
 		numbers[i] = findNumberArea(parts_changeable[i], -20, Size(2, 15), Size(2, 300));
 
-	/*//查看
+	//查看，将展示选择出单个数字区域的情况，用绿色框框出
 	char str[6] = "test0";
 	for (int i = 0; i < numbers.size(); i++)
 	{
 		for(int j = 0; j < numbers[i].size(); j++)
 		{
-			rectangle(parts[i], numbers[i][j], Scalar(CV_RGB(0, 255, 0)), 1, 8, 0);
+			rectangle(parts_changeable[i], numbers[i][j], Scalar(CV_RGB(0, 255, 0)), 1, 8, 0);
 		}
 		str[4] = (char)(i + '0');
-		imshow(str, parts[i]);
-	}*/
+		imshow(str, parts_changeable[i]);
+	}
 	
-	//分离单个数字，注意：一定要注释掉上方的查看部分，否则会有边框
+	//分离单个数字
 	vector<Mat> numbersArea0 = separateNumberArea(parts[0], numbers[0]);
 	vector<Mat> numbersArea1 = separateNumberArea(parts[1], numbers[1]);
 	vector<Mat> numbersArea2 = separateNumberArea(parts[2], numbers[2]);
 
-	//查看
-	//char strNum[4] = "2-0";
-	//for (int i = 0; i < numbersArea1.size(); i++)
-	//{
-	//	strNum[2] = (char)(i+'0');
-	//	namedWindow(strNum, WINDOW_NORMAL);
-	//	imshow(strNum, numbersArea1[i]);
-	//}
-	//for(int j = 0; j < numbers[1].size(); j++)
-	//{
-	//	rectangle(parts[1], numbers[1][j], Scalar(CV_RGB(0, 255, 0)), 1, 8, 0);
-	//}
-	//imshow("part1", parts[1]);
-
-
-	//imshow("parts0", parts[0]);
-	//imshow("parts1", parts[1]);
-	//imshow("parts2", parts[2]);
-	//imshow("test", src_changeable);
-	//printf("%d,%d", stringArea[0].x, stringArea[0].y);
-
+	//查看：分别展示单个数字区域，这将打开很多窗口，注意
+	char strNum[4] = "2-0";
+	for (int i = 0; i < numbersArea1.size(); i++)
+	{
+		strNum[2] = (char)(i+'0');
+		namedWindow(strNum, WINDOW_NORMAL);
+		imshow(strNum, numbersArea1[i]);
+	}
+	/*                           图片分割部分结束                                */
+	/**************************************************************************/
+	/**************************************************************************/
+	/*                 制作匹配模板，已经制作完成了，故下方全注释了                   */
 	char modulename[40] = "./img/module0.jpg";
 	//generage template
 	/*char modulename2[40] = "./img/img/module0.jpg";
@@ -96,8 +91,9 @@ int main()
 		erode(temp, temp, getStructuringElement(CV_SHAPE_ELLIPSE, Size(7, 7)));
 		imwrite(modulename, temp);
 	}*/
-
-
+	/**************************************************************************/
+	/**************************************************************************/
+	/*                       匹配模板并输出结果到控制台                            */
 	vector<Mat> modules;
 	for (int ii = 0;ii < 9;ii++) {
 		modulename[12] = '0' + ii;
@@ -115,6 +111,7 @@ int main()
 	cout << endl;
 
 	system("pause");
-	//waitKey(0);
+	waitKey(0);
+
 	return 0;
 } 
